6f75f30a0c7d69d5def9e96fe7caf156ade4c6b5
DigoRM <82246925+DigoRM@users.noreply.github.com>
2023-01-31 20:36:59 -0300

    Update settings.py

--- SimpleForum/settings.py
+++ SimpleForum/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 
 
@@ -134,4 +133,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
