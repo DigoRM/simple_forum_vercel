8c6fccbae0e68d706b709d65c9ce3bd817147af1
DigoRM <82246925+DigoRM@users.noreply.github.com>
2023-02-01 11:38:05 -0300

    Update settings.py

--- SimpleForum/settings.py
+++ SimpleForum/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    'blog/static/',
+        os.path.join(BASE_DIR, "static"),
     ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
