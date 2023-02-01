f21f8000292acba602234b3f4ae8077e3454177e
DigoRM <82246925+DigoRM@users.noreply.github.com>
2023-01-31 23:19:46 -0300

    Update urls.py

--- SimpleForum/urls.py
+++ SimpleForum/urls.py
@@ -32,3 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
+
+from django.conf.urls.static import static
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
