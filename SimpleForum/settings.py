ba91372c8476c6b1b1d7fdfdbdf46b5326b753b5
DigoRM <rodrigormarcolino@gmail.com>
2023-02-01 21:11:42 -0300

    used collectstatic

--- SimpleForum/settings.py
+++ SimpleForum/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'whitenoise.runserver_nostatic',    
 ]
 
 MIDDLEWARE = [
@@ -127,9 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
