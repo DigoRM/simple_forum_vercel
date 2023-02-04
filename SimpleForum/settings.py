e6d40358c65171e613ff00482297acdc9a0b3de8
DigoRM <rodrigormarcolino@gmail.com>
2023-02-03 23:38:51 -0300

    button ant category's title

--- SimpleForum/settings.py
+++ SimpleForum/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-#CSRF_TRUSTED_ORIGINS = ['https://rmtechblog.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://rmtechblog.up.railway.app']
 
 
 # Application definition
